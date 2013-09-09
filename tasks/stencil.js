ebd6a0c93b7b71b24ba529a61abef615ff8bdf08
Elise Hein <elisehein@gmail.com>
2013-09-09 17:37:59 +0100

    Validate that a 1:1 mapping is used

--- tasks/stencil.js
+++ tasks/stencil.js
@@ -2,49 +2,111 @@
  * grunt-stencil
  * https://github.com/cambridge-healthcare/grunt-stencil
  *
- * Copyright (c) 2013 Elise Hein
+ * Copyright (c) 2013 Cambridge Healthcare
  * Licensed under the MIT license.
  */
 
 'use strict';
+var dot = require('dot');
+var _ = require('underscore');
+var lib = require('../lib/lib.js');
 
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
-
   grunt.registerMultiTask('stencil', 'A Grunt plugin to generate static HTML files from given components.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
+
+    // Define default options
     var options = this.options({
-      punctuation: '.',
-      separator: ', '
+      dot_template_settings: {},
+      dot_it_object: {
+        file_lists: {}
+      },
+      templates_folder: '',
     });
+
+    // Build it object
+    var it = options.dot_it_object;
+    it.file_lists = lib.expand_file_lists(it.file_lists);
+
+    // Override default dot templateSettings with given options
+    _.extend(dot.templateSettings, options.dot_template_settings);
+
+    // Specify where to find templates
+    var get_template_path = lib.find_closest_match.bind(null, options.templates_folder);
 
     // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+    // mapping.src already contains only existing files
+    this.files.forEach(function(mapping) {
 
-      // Handle options.
-      src += options.punctuation;
+      // Check there is a 1:1 src-dest mapping
+      validate_mapping(mapping);
 
       // Write the destination file.
-      grunt.file.write(f.dest, src);
+      //grunt.file.write(mapping.dest, src);
 
       // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+      grunt.log.writeln('File ' + mapping.dest.cyan + ' created.');
     });
   });
+
+  // Compile a single template file to a single output destination file
+  function compile_template (get_template_path, it, dest, src_path) {
+    var output_file = grunt.file.isDir(dest)
+                      ? lib.html_filepath(dest, src_path)
+                      : dest;
+    var src = grunt.file.read(src_path);
+
+    var meta = lib.try_and_report(function () {
+      return lib.meta_data(src);
+    }, 'Error parsing JSON header in', src_path);
+
+    _.extend(it, meta);
+
+    // Compile document
+    var document = lib.try_and_report(function () {
+      return dot.template(lib.content(src))(it);
+    }, 'Error processing template of', src_path);
+
+    // Wrap the document in it's template
+    document = lib.try_and_report(function () {
+      return wrap(get_template_path, document, it);
+    }, 'Error processing template', it.template);
+
+
+    grunt.file.write(output_file, document);
+
+    // Log
+    grunt.log.writeln('File ' + output_file.cyan + ' created.');
+  }
+
+  function wrap (get_template_path, document, it) {
+    // If the it object now has a 'template' option,
+    // We need to first compile the pages to a 'document' cache
+
+    // The dot template is now the new file,
+    // which gets access to the compiled original file
+    var template;
+
+    if (it.template) {
+      _.extend(it, { document: document });
+
+      template = lib.fold_fns(it.template,
+                              get_template_path,
+                              grunt.file.read,
+                              dot.template);
+
+      return template(it);
+    } else {
+      return document;
+    }
+  }
+
+  function validate_mapping (mapping) {
+    if (mapping.src.length > 1) {
+      grunt.fail.warn("You are attempting to compile a single output file " +
+                      "from several input files.\nEither amend the src declaration," +
+                      "or use a dynamic file mapping instead.");
+    }
+  }
 
 };
