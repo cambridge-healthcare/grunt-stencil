c702eba1ac5d075ee482d6de68d53c3a2ee80531
Elise Hein <elisehein@gmail.com>
2013-09-10 13:09:24 +0100

    Generic test to compare expected files to generated files

--- test/stencil_test.js
+++ test/stencil_test.js
@@ -27,21 +27,21 @@ exports.stencil = {
     // setup here if necessary
     done();
   },
-  default_options: function(test) {
+  selfstanding: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/default_options');
-    var expected = grunt.file.read('test/expected/default_options');
-    test.equal(actual, expected, 'should describe what the default behavior is.');
+    var actual = grunt.file.read('tmp/selfstanding.html');
+    var expected = grunt.file.read('test/expected/selfstanding.html');
+    test.equal(actual, expected, '');
 
     test.done();
   },
   custom_options: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/custom_options');
-    var expected = grunt.file.read('test/expected/custom_options');
-    test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
+    var actual = grunt.file.read('tmp/with_template.html');
+    var expected = grunt.file.read('test/expected/with_template.html');
+    test.equal(actual, expected, '');
 
     test.done();
   },
