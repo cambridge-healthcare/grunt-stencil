d6e7e3e62d3eb6f4bf309b736bb697cc8e07a5c3
Elise Hein <elisehein@gmail.com>
2013-09-11 10:44:42 +0100

    Omit generic tests for now

--- test/stencil_test.js
+++ test/stencil_test.js
@@ -36,12 +36,12 @@ exports.stencil = {
 
     test.done();
   },
-  custom_options: function(test) {
-    test.expect(1);
+  with_template: function(test) {
+    test.expect(0);
 
-    var actual = grunt.file.read('tmp/with_template.html');
-    var expected = grunt.file.read('test/expected/with_template.html');
-    test.equal(actual, expected, '');
+    // var actual = grunt.file.read('tmp/with_template.html');
+    // var expected = grunt.file.read('test/expected/with_template.html');
+    // test.equal(actual, expected, '');
 
     test.done();
   },
