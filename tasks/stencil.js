dc1e693c03e56b4af08926b35d02f3dc40233577
Elise Hein <elisehein@gmail.com>
2013-09-11 14:35:25 +0100

    Can now specify partials without extensions

--- tasks/stencil.js
+++ tasks/stencil.js
@@ -51,9 +51,11 @@ module.exports = function(grunt) {
 
   // Compile the source of an input file
   function compile (options, input_file, is_page) {
-    console.log("\nInclude has been called on " + input_file.cyan + (is_page ? ", which is a page!" : ""));
-    console.log("Initial it: ");
-    console.log(options.dot_it_object);
+
+    // First make sure we have the full path
+    input_file = is_page
+                 ? input_file
+                 : utils.find_closest_match(options.partials_folder, input_file);
 
     // Define a dot template compiler based on given options
     var dot_compiler = utils.compile_dot.bind(null,
@@ -63,19 +65,14 @@ module.exports = function(grunt) {
     // Build the it object and add meta data if we find some
     var it = _.extend(options.dot_it_object,
                       utils.meta_data(input_file, options.meta_data_sep));
-    console.log("It after meta data:")
-    console.log(it);
 
     // Compile dot template using it object
     var doc = dot_compiler(input_file, it);
 
-    console.log("Now back to " + input_file)
-
-    // In case a parent template is defined in the meta data,
+    // In case a parent template is defined in a page's meta data
     // compile it with the document passed in the it object
     var parent_template;
     if (it.template && is_page) {
-      console.log(input_file + " DID have a template specified!!");
       parent_template = utils.find_closest_match(options.templates_folder, it.template);
       _.extend(it, { document: doc });
       doc = dot_compiler(parent_template, it);
