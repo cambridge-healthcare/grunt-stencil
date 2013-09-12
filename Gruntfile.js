0696a149cd49d5dfb8d8cb1ac13b9b3c9b9523c1
Elise Hein <elisehein@gmail.com>
2013-09-12 17:05:47 +0100

    We don't need node-unit

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,7 +57,6 @@ module.exports = function(grunt) {
         ]
       }
     }
-
   });
 
   // Actually load this plugin's task(s).
@@ -66,13 +65,12 @@ module.exports = function(grunt) {
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
   //grunt.registerTask('test', ['clean', 'stencil', 'nodeunit']);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['clean', 'jshint', 'stencil', 'stencil-tests']);
+  grunt.registerTask('default', ['clean', 'jshint', 'stencil']);
 
 };
