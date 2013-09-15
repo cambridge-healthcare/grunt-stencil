56a515cd0718a464b95b8718d5d5494653f49a8a
Szymon Witamborski <szywon@live.com>
2013-09-15 19:25:12 +0100

    Page and include all green.

--- spec/page_spec.js
+++ spec/page_spec.js
@@ -1,25 +1,35 @@
-var _ = require('underscore');
-
-function random_word () {
-  var word =
-    _.shuffle('qwertyuiopasdfghjklzxcvbnm').join('')
-    .substring(0, 5 + Math.round(Math.random() * 10));
-  return word;
-}
+var randoms = require('./randoms');
 
 describe("page", function () {
-  var separator = random_word();
+  var separator = randoms.word();
 
   var source = require('../lib/source')(separator);
-  var page = require('../lib/page')(source);
+
+  var include = require('../lib/include')({
+    read: function (name) { return partials[name] }
+  });
+
+  var page = require('../lib/page')({
+    source: source,
+    include: include
+  });
+
+  var partials = {
+    asdf: randoms.word(),
+    qwer: '{"a":1}'
+  };
+
+  function read_partial_source (path) {
+    return partials[path];
+  }
 
   it("compiles dot template", function () {
-    var text = random_word();
+    var text = randoms.word();
     expect(page('{{= "' + text + '" }}')).toEqual(text);
   });
 
   it("passes header to template", function () {
-    var title = random_word();
+    var title = randoms.word();
     var header = JSON.stringify({
       title: title
     });
@@ -27,5 +37,16 @@ describe("page", function () {
     var content = '{{= it.title }}';
 
     expect(page(header + separator + content)).toEqual(title);
+  });
+
+  it("includes partials", function () {
+    var content = '{{= it.include("asdf") }}';
+
+    expect(page(content)).toEqual(partials.asdf);
+  });
+
+  it("has partial's header fields available", function () {
+    var content = '{{= it.include("qwer").a }}';
+    expect(page(content)).toEqual('1');
   });
 });
