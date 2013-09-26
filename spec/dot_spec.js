5bc9c184708af78236978a940044a996e7732d6d
Elise Hein <elisehein@gmail.com>
2013-09-26 16:01:49 +0100

    Add tests for it extender

--- spec/dot_spec.js
+++ spec/dot_spec.js
@@ -11,12 +11,28 @@ describe('dot_compiler', function() {
   };
 
   var dot = new (require('../lib/dot_compiler'))({
-    it: {title: "asdf"},
+    it: {title: 'asdf'},
     template_settings: {}
   });
+
+  beforeEach(function() {
+    dot.it = {title: 'asdf'};
+  })
 
   it('compiles dot templates', function() {
     var content = '{{= "' + fixt.word + '" }}';
     expect(dot.compile(content)).toEqual(fixt.word);
+  });
+
+  describe('extend_it', function() {
+    it('can add parameters to the it object', function() {
+      dot.extend_it({qwer: 22});
+      expect(dot.it).toEqual({title: 'asdf', qwer: 22});
+    });
+
+    it('can override values in the it object', function() {
+      dot.extend_it({asdf: 1}, {title: 2});
+      expect(dot.it).toEqual({title: 2, asdf: 1});
+    });
   });
 });
