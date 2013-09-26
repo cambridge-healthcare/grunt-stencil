bb25f6f01de834a67fe24650cdd7d7c5433a9f0a
Elise Hein <elisehein@gmail.com>
2013-09-26 19:24:56 +0100

    Clean up page

--- spec/page_spec.js
+++ spec/page_spec.js
@@ -1,9 +1,8 @@
 var random = require('./random');
 var parse = require('../lib/parse')('\n\n');
+var page = require('../lib/page');
 
 describe("page", function () {
-
-  var page = require('../lib/page');
 
   var compilers = [
     {compile: function (x) { return "Compiling " + x; }},
@@ -32,17 +31,6 @@ describe("page", function () {
     expect(compile(input).toString()).toEqual("%Compiling " + input + "%!");
   });
 
-  it("uses dot's settings when compiling with dot", function() {
-    input = "{{= x.variable }}";
-    var compile = page({
-      compilers: [new (require('../lib/dot_compiler'))({
-        it: {variable: 123},
-        template_settings: {varname: "x"}
-      })],
-      parser: parse
-    });
-    expect(compile(input).toString()).toEqual("123");
-  });
 
   it("has access to fields in the input's header", function() {
     input = JSON.stringify({title: "asdf"}) + "\n\nContent";
@@ -50,56 +38,24 @@ describe("page", function () {
     expect(compile(input).title).toEqual("asdf");
   });
 
+  describe("when a dot compiler is given", function() {
+    it("uses given dotvar object and template settings", function() {
+      input = "{{= x.variable }}";
+      var compile = page({
+        compilers: [new (require('../lib/dot_compiler'))({
+          dotvar: {variable: 123},
+          template_settings: {varname: "x"}
+        })],
+        parser: parse
+      });
+      expect(compile(input).toString()).toEqual("123");
+    });
+  });
+
   describe("when no compilers are given", function() {
     it("returns the same string", function() {
       var compile = page({parser: parse});
       expect(compile(input).toString()).toEqual(input);
-    });
-  });
-
-
-//===============================================
-
-  xit("compiles dot files", function () {
-    var text = random.word();
-    expect(page('{{= "' + text + '" }}')).toEqual(text);
-  });
-
-  xit("passes data from the header to the content", function () {
-    var title = random.word();
-    var header = JSON.stringify({
-      title: title
-    });
-
-    var content = '{{= it.title }}';
-    expect(page(header + content)).toEqual(title);
-  });
-
-  xdescribe("when it has an 'include' statement", function() {
-
-    it("includes a partial", function () {
-      var content = '{{= it.include("asdf") }}';
-      expect(page(content)).toEqual(partials.asdf);
-    });
-
-    it("has acces to meta data from the included partial", function () {
-      var content = '{{= it.include("qwer").a }}';
-      expect(page(content)).toEqual('1');
-    });
-
-    it("compiles the partial with doT", function () {
-      var content = '{{= it.include("dotted") }}';
-      expect(page(content)).toEqual('asdf');
-    });
-
-    it("can specify extra parameters to be passed to the partial", function () {
-      var content = '{{= it.include("dotted_w_params", {title:"asdf"}) }}';
-      expect(page(content)).toEqual('asdf');
-    });
-
-    it("can process other include statements inside this one", function() {
-      var content = '{{= it.include("nested_include") }} Hello';
-      expect(page(content)).toEqual(partials.asdf + ' Hello');
     });
   });
 });
