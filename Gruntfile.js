5df0acfaf1f4691c61237a9868287866cdbb3f4d
Elise Hein <elisehein@gmail.com>
2013-09-27 17:30:14 +0100

    Separate tasks for each case

--- Gruntfile.js
+++ Gruntfile.js
@@ -87,7 +87,7 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     stencil: {
-      fixtures: {
+      all: {
         options: {
           partials: 'spec/includes'
         },
@@ -96,7 +96,88 @@ module.exports = function (grunt) {
           cwd: 'spec/fixtures',
           src: '*.html',
           dest: 'tmp',
-          ext: '.html'
+          ext: '.html',
+          flatten: true
+        }]
+      },
+
+      dot_only: {
+        options: {
+          partials: 'spec/includes'
+        },
+        files: [{
+          expand: true,
+          src: 'spec/fixtures/dot_only.dot.html',
+          dest: 'tmp',
+          ext: '.html',
+          flatten: true
+        }]
+      },
+
+      include_with_params: {
+        options: {
+          partials: 'spec/includes'
+        },
+        files: [{
+          expand: true,
+          src: 'spec/fixtures/include_with_params.dot.html',
+          dest: 'tmp',
+          ext: '.html',
+          flatten: true
+        }]
+      },
+
+
+      includes_partial: {
+        options: {
+          partials: 'spec/includes'
+        },
+        files: [{
+          expand: true,
+          src: 'spec/fixtures/includes_partial.dot.html',
+          dest: 'tmp',
+          ext: '.html',
+          flatten: true
+        }]
+      },
+
+      nested_include: {
+        options: {
+          partials: 'spec/includes'
+        },
+        files: [{
+          expand: true,
+          src: 'spec/fixtures/nested_include.dot.html',
+          dest: 'tmp',
+          ext: '.html',
+          flatten: true
+        }]
+      },
+
+
+      with_header: {
+        options: {
+          partials: 'spec/includes'
+        },
+        files: [{
+          expand: true,
+          src: 'spec/fixtures/with_header.md.dot.html',
+          dest: 'tmp',
+          ext: '.html',
+          flatten: true
+        }]
+      },
+
+      circular: {
+        options: {
+          partials: 'spec/includes'
+        },
+        files: [{
+          expand: true,
+          src: 'spec/includes/example_with_circular_include.dot.html',
+          dest: 'tmp',
+          ext: '.html',
+          flatten: true
         }]
       }
     }
