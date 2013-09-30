1e8834c647e23731aea213ca0e7d829da53d35a3
Szymon Witamborski <szywon@szywon.pl>
2013-09-30 10:43:05 +0100

    Dot compiler: eliminate side effects of one-off configuration and remove unused code.

--- spec/dot_spec.js
+++ spec/dot_spec.js
@@ -23,16 +23,4 @@ describe('dot_compiler', function() {
     var content = '{{= "' + fixt.word + '" }}';
     expect(dot.compile(content)).toEqual(fixt.word);
   });
-
-  describe('extend_it', function() {
-    it('can add parameters to the it object', function() {
-      dot.extend_dotvar({qwer: 22});
-      expect(dot.dotvar).toEqual({title: 'asdf', qwer: 22});
-    });
-
-    it('can override values in the it object', function() {
-      dot.extend_dotvar({asdf: 1}, {title: 2});
-      expect(dot.dotvar).toEqual({title: 2, asdf: 1});
-    });
-  });
 });
